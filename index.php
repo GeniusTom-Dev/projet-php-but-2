a37aa49e9fd68f77ee0b4b00d25a16e4b5d4afed
maxime <maxime.jauras@etu.univ-amu.fr>
2023-09-21 07:43:37 +0000

    enlarged or shrunk the post when clicking on the arrows image using javascript.

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 
 </div>
 
-<article class="article">
+<article id="article" class="article">
     <header class="article-header">
         <p>@Profile_name</p>
         <p>Follow | 1K followers</p>
-        <img src="/html/images/fleches.svg" alt="Agrandir poste" class="article-image">
+        <img id="growArrow" src="/html/images/fleches.svg" alt="growArrow" class="article-image">
         <img src="/html/images/profile-removebg-preview.png" alt="PP" class="article-image2">
     </header>
     <main>
@@ -26,6 +26,8 @@
         <p>Tags : <a href="#">Tag1</a>, <a href="#">Tag2</a>, <a href="#">Tag3</a></p>
     </footer>
 </article>
+
+<script src="/html/script/script.js"></script>
 </body>
 </html>
 
