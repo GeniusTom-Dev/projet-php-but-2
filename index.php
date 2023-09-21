ea4c2e41db60638e74b202db01c1e63d64cd9e6d
maxime <maxime.jauras@etu.univ-amu.fr>
2023-09-21 06:45:48 +0000

    creation of an article which will contain the post of our site.

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma Page</title>
+    <link rel="stylesheet" type="text/css" href="/html/styles/index.css">
+</head>
+<body>
+
+<div class="nav">
+
+</div>
+
+<article class="article">
+    <header class="article-header">
+        <p>@Profile_name</p>
+        <p>Follow | 1K followers</p>
+        <img src="/html/images/fleches.svg" alt="Agrandir poste" class="article-image">
+        <img src="/html/images/profile-removebg-preview.png" alt="PP" class="article-image2">
+    </header>
+    <main>
+        <h1>Titre poste</h1>
+        <p>Ceci est un exemple de post Twitter-like.</p>
+        <p>Vous pouvez ajouter plus de contenu ici.</p>
+    </main>
+    <footer>
+        <p>Tags : <a href="#">Tag1</a>, <a href="#">Tag2</a>, <a href="#">Tag3</a></p>
+    </footer>
+</article>
+</body>
+</html>
 
