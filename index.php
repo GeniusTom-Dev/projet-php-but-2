1144d4b6a79082d8e684e29b33402f7033f60318
THIEL-Maxime-2225084a <maxime.thiel@etu.univ-amu.fr>
2023-09-28 11:53:29 +0200

    Barre de recherche en CSS

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="/html/styles/index.css">
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-        <input id="searchBar" placeholder="Search someone or something..."/>
+        <div class="flex justify-center">
+        <input id="searchBar" class="searchBar" placeholder="Search someone or something..."/>
     </body>
 </html>
