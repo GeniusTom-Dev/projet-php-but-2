846cf6b49cb1d832b82ba0a2d352423657b581d9
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-09-28 10:18:00 +0200

    Designarticle

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma Page</title>
+    <link rel="stylesheet" type="text/css" href="/html/styles/index.css">
+</head>
+<body>
+
+<div class="nav">
+
+</div>
+
+<article class="article">
+    <header class="article-header">
+        <p>@Profile_name</p>
+        <p>Follow | 1K followers</p>
+        <img id="growArrow" src="/html/images/fleches.svg" alt="growArrow" class="article-image">
+        <img src="/html/images/profile-removebg-preview.png" alt="PP" class="article-image2">
+    </header>
+    <main>
+        <h1>Titre poste</h1>
+        <p>Ceci est un exemple de post Twitter-like.</p>
+        <p>Vous pouvez ajouter plus de contenu ici.</p>
+    </main>
+    <footer>
+        <p>Tags : <a href="#">Tag1</a>, <a href="#">Tag2</a>, <a href="#">Tag3</a></p>
+    </footer>
+</article>
+
+<div class="image-container">
+    <img id="arrowLeft" src="/html/images/arrow-left-solid.svg" alt="arrowLeft" class="arrowLeft">
+    <img id="heartRegular" src="/html/images/heart-regular.svg" alt="heart" class="heartRegular">
+    <img id="bookmarkRegular" src="/html/images/bookmark-regular.svg" alt="bookmark" class="bookmarkRegular">
+    <img id="arrowRight" src="/html/images/arrow-right-solid.svg" alt="arrowRight" class="arrowRight">
+</div>
+
+<script src="/html/script/script.js"></script>
+</body>
+</html>
+
