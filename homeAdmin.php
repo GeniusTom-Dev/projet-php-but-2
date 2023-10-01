8776041211b3e334865537bf45d988594621c6a8
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-10-01 12:00:47 +0200

    Système d'affichage de tous les paramètres de GET dans la barre d'adresse

--- homeAdmin.php
+++ homeAdmin.php
@@ -7,6 +7,20 @@ if (isset($_SESSION['suid']) && $_SESSION['admin'] == true){
     require_once 'autoloads/adminAutoloader.php';
     checkTab();
     checkSort();
+
+    // Système de conservation d'élément de GET précédents qui n'ont pas été envoyés via la nouvelle requête mais sont présents et doivent être mentionnés.
+    if(http_build_query($_GET) == ''){
+        $url = '/projet-php-but-2/homeAdmin.php';
+        if($_SERVER['REQUEST_URI'] != $url){
+            header('Location: ' . $url);
+        }
+    }
+    else {
+        $url = '/projet-php-but-2/homeAdmin.php?' . http_build_query($_GET);
+        if($_SERVER['REQUEST_URI'] != $url){
+            header('Location: ' . $url);
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -18,6 +32,9 @@ if (isset($_SESSION['suid']) && $_SESSION['admin'] == true){
     <header></header>
     <?php require 'views/nav-admin.php';?>
     <?php echo '<p>', $_SERVER['REQUEST_URI'], '</p>'; ?>
+    <?php echo '<p>', http_build_query($_GET), '</p>'; ?>
+<!--    --><?php //$_SERVER['REQUEST_URI'] = '/projet-php-but-2/homeAdmin.php?' . http_build_query($_GET); ?>
+<!--    --><?php //echo '<p>', $_SERVER['REQUEST_URI'], '</p>'; ?>
     <section>
         <?php homeReload();?>
     </section>
