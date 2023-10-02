9012b0e9bfbf0deac56a7ec96c69a837db5e4c65
t21203175 <maxime.thiel@etu.univ-amu.fr>
2023-10-02 10:02:59 +0200

    fenêtre des tendances

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-        <div class="flex justify-center">
         <input id="searchBar" class="searchBar" placeholder="Search someone or something..."/>
+        <div class="trendsWindow">
+            <p><span> TEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTE </span></p>
+        <div/>
     </body>
 </html>
