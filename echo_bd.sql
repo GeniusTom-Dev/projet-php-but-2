f7e41ca40d41cdb5f534b6261f393fa09b1bb5f4
Mathieu Leroux <113862047+Badlix@users.noreply.github.com>
2023-10-02 11:40:29 +0200

    New script

---
+++ echo_bd.sql
@@ -0,0 +1,271 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql-echo.alwaysdata.net
+-- Generation Time: Oct 01, 2023 at 12:23 PM
+-- Server version: 10.6.14-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `echo_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `belongs_to`
+--
+
+CREATE TABLE `belongs_to` (
+  `POST_ID` int(11) NOT NULL,
+  `TOPIC_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `COMMENT_ID` int(11) NOT NULL,
+  `POST_ID` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL,
+  `CONTENT` varchar(500) NOT NULL,
+  `DATE_POSTED` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `POST_ID` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follows`
+--
+
+CREATE TABLE `follows` (
+  `USER_ID_FOLLOWER` int(11) NOT NULL,
+  `USER_ID_FOLLOWED` int(11) NOT NULL,
+  `SINCE_WHEN` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `POST_ID` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `POST_ID` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL,
+  `TITLE` varchar(25) DEFAULT NULL,
+  `CONTENT` varchar(1000) NOT NULL,
+  `DATE_POSTED` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE `topics` (
+  `TOPIC_ID` int(11) NOT NULL,
+  `NAME` varchar(25) NOT NULL,
+  `DESCRIPTION` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `USER_ID` int(11) NOT NULL,
+  `USERNAME` varchar(25) NOT NULL,
+  `USER_EMAIL` varchar(75) NOT NULL,
+  `USER_PWD` varchar(50) NOT NULL,
+  `IS_ACTIVATED` tinyint(1) NOT NULL,
+  `IS_ADMIN` tinyint(1) NOT NULL,
+  `USER_CREATED` date NOT NULL,
+  `USER_LAST_CONNECTION` date NOT NULL,
+  `USER_PROFIL_PIC` varchar(100) DEFAULT NULL,
+  `USER_BIO` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `belongs_to`
+--
+ALTER TABLE `belongs_to`
+  ADD PRIMARY KEY (`POST_ID`,`TOPIC_ID`),
+  ADD KEY `FK2_BELONGS_TO` (`TOPIC_ID`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`COMMENT_ID`),
+  ADD KEY `FK1_COMMENT` (`POST_ID`),
+  ADD KEY `FK2_COMMENT` (`USER_ID`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`POST_ID`,`USER_ID`),
+  ADD KEY `FK2_FAVORITE` (`USER_ID`);
+
+--
+-- Indexes for table `follows`
+--
+ALTER TABLE `follows`
+  ADD PRIMARY KEY (`USER_ID_FOLLOWER`,`USER_ID_FOLLOWED`),
+  ADD KEY `FK1_FOLLOW` (`USER_ID_FOLLOWED`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`USER_ID`,`POST_ID`),
+  ADD KEY `FK1_LIKES` (`POST_ID`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`POST_ID`),
+  ADD KEY `FK_CONSTRAINT` (`USER_ID`);
+
+--
+-- Indexes for table `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`TOPIC_ID`),
+  ADD UNIQUE KEY `NAME` (`NAME`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`USER_ID`),
+  ADD UNIQUE KEY `USERNAME` (`USERNAME`),
+  ADD UNIQUE KEY `USER_EMAIL` (`USER_EMAIL`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `comments`
+  MODIFY `COMMENT_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `posts`
+  MODIFY `POST_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `topics`
+  MODIFY `TOPIC_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `belongs_to`
+--
+ALTER TABLE `belongs_to`
+  ADD CONSTRAINT `FK1_BELONGS_TO` FOREIGN KEY (`POST_ID`) REFERENCES `posts` (`POST_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK2_BELONGS_TO` FOREIGN KEY (`TOPIC_ID`) REFERENCES `topics` (`TOPIC_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `FK1_COMMENT` FOREIGN KEY (`POST_ID`) REFERENCES `posts` (`POST_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK2_COMMENT` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `FK1_FAVORITE` FOREIGN KEY (`POST_ID`) REFERENCES `posts` (`POST_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK2_FAVORITE` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `follows`
+--
+ALTER TABLE `follows`
+  ADD CONSTRAINT `FK1_FOLLOW` FOREIGN KEY (`USER_ID_FOLLOWED`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK2_FOLLOW` FOREIGN KEY (`USER_ID_FOLLOWER`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `FK1_LIKES` FOREIGN KEY (`POST_ID`) REFERENCES `posts` (`POST_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK2_LIKES` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `FK_CONSTRAINT` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
