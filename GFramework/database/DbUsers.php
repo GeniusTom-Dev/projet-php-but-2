33d51afd6c634765b443e69dc66ed8a840f58fef
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-10-06 09:54:25 +0200

    Resolve error due to merge

--- GFramework/database/DbUsers.php
+++ GFramework/database/DbUsers.php
@@ -31,7 +31,7 @@ class DbUsers{
      * @return GReturn
      * Used when you need to filter the table according to several non-unique key attributes
      */
-    public function select_SQLResult(?string $usernameLike, ?bool $isAdmin, ?bool $isActivated, ?int $limit = null, ?int $page, ?string $sort) : GReturn{
+    public function select_SQLResult(?string $usernameLike, ?bool $isAdmin, ?bool $isActivated, ?int $limit, ?int $page, ?string $sort) : GReturn{
         $request = "SELECT * FROM " . $this->dbName;
         $conditions = [];
         if (!is_null($usernameLike)) {
@@ -44,7 +44,7 @@ class DbUsers{
             $conditions[] = "IS_ACTIVATED = $isActivated";
         }
         if (!empty($conditions)) {
-            $request .= "WHERE " . implode(" AND ", $conditions);
+            $request .= " WHERE " . implode(" AND ", $conditions);
         }
         // Sorting result and limiting size for pagination
         $request .= " " . $this->getSortInstruction($sort);
