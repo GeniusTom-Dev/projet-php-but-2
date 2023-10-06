f5c883ac0cf90ee96e66fb28d24f07e0d18d5888
t21203175 <maxime.thiel@etu.univ-amu.fr>
2023-10-06 11:36:22 +0200

    modif fenêtre categ

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
     </head>
     <body>
         <input id="searchBar" class="searchBar" placeholder="Search someone or something..."/>
-        <div class="trendsWindow">
-            <p><span> TEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTETEXTE </span></p>
+        <div class="categWindow">
+            <p><span> CATEGORIES </span></p>
+            <div class="categRect">
+                <div> Un </div>
+                <div> Deux </div>
+                <div> Trois </div>
+            </div>
         <div/>
     </body>
 </html>
