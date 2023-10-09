df28ab55c2c8025f12966562723ffa47d1225be8
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-10-09 14:59:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
         <div class="categWindow">
             <p><span> CATEGORIES </span></p>
             <div class="categRect">
-                <div> Un </div>
-                <div> Deux </div>
-                <div> Trois </div>
+                <span> @Un : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem diam, molestie a convallis non, 
+                ullamcorper vitae velit. Aliquam interdum rhoncus nunc, in malesuada libero semper a. Pellentesque habitant 
+                morbi tristique senectus et netus et malesuada fames ac turpis egestas.</span>
+            </div>
+            <div class="categRect">
+                <span> @Deux : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem diam, molestie a convallis non, 
+                ullamcorper vitae velit. Aliquam interdum rhoncus nunc, in malesuada libero semper a. Pellentesque habitant 
+                morbi tristique senectus et netus et malesuada fames ac turpis egestas.</span>
+            </div>
+            <div class="categRect">
+                <span> @Trois : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem diam, molestie a convallis non, 
+                ullamcorper vitae velit. Aliquam interdum rhoncus nunc, in malesuada libero semper a. Pellentesque habitant 
+                morbi tristique senectus et netus et malesuada fames ac turpis egestas.</span>
+            </div>
+            <div class="categRect">
+                <span> @Quatre : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sem diam, molestie a convallis non, 
+                ullamcorper vitae velit. Aliquam interdum rhoncus nunc, in malesuada libero semper a. Pellentesque habitant 
+                morbi tristique senectus et netus et malesuada fames ac turpis egestas.</span>
             </div>
         <div/>
     </body>
