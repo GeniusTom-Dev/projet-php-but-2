f17e7c0a93cc4c3321e52fa63271ea10cf73b214
GeniusTom <tomeven13@gmail.com>
2023-10-24 18:04:29 +0200

    add functions validatePassword.php & start register functions

--- GFramework/autoloader.php
+++ GFramework/autoloader.php
@@ -1,12 +1,14 @@
 <?php
 require_once("database/Database.php");
-require_once("database/DbTopics.php");
+require_once("database/dbTopics.php");
 require_once("database/DbUsers.php");
-require_once("database/DbPosts.php");
-require_once("database/DbComments.php");
+require_once("database/dbPosts.php");
+require_once("database/dbComments.php");
 require_once("utilities/GReturn.php");
 require_once("utilities/CannotDoException.php");
 
-$db = new Database('mysql-echo.alwaysdata.net','echo_mathieu','130304leroux','echo_bd');
+$db = new Database('mysql-echo.alwaysdata.net','echo_mathieu','130304leroux','echo_bd_test');
 $dbConn = $db->getConnection()->getContent();
+
+define('__ROOT__', realpath(dirname(__DIR__) . "/"))
 ?>
--- html/pages/admin/register.php
+++ html/pages/admin/register.php
@@ -1,6 +1,10 @@
 <?php
+include "../../../GFramework/autoloader.php";
+require __ROOT__. '/GFramework/functions/validatePassword.php';
 
 if(isset($_POST["email"]) && isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["confirmPassword"])){
+    $dbUsers = new DbUsers($dbConn);
+
     $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
@@ -8,6 +12,14 @@ if(isset($_POST["email"]) && isset($_POST["username"]) && isset($_POST["password
 
     if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
         $emailError = "L'adresse mail n'est pas au bon format.";
+    }else{
+        if($dbUsers->isEmailAlreadyUsed($email)){
+            $email = "Mail déjà utilisé.";
+        }
+    }
+
+    if($dbUsers->isUsernameAlreadyUsed($username)){
+        $usernameError = "Nom d'utilisateur déjà utilisé.";
     }
 
     if(validatePassword($password) === false){
@@ -30,6 +42,7 @@ if(isset($_POST["email"]) && isset($_POST["username"]) && isset($_POST["password
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
     <title>Register | Echo</title>
 </head>
 <!--bg-[#EDE9FF]-->
@@ -40,8 +53,15 @@ if(isset($_POST["email"]) && isset($_POST["username"]) && isset($_POST["password
     <h1 class="text-[#b2a5ff] text-2xl">Inscription</h1>
     <input name="email" type="text" placeholder="Email" class="w-full rounded-md text-lg outline-none border-2 p-1">
     <input name="username" type="text" placeholder="Nom d'utilisateur" class="w-full rounded-md text-lg outline-none border-2 p-1">
-    <input name="password" type="password" placeholder="Password" class="w-full rounded-md text-lg outline-none border-2 p-1">
+    <input name="password" type="password" id="mainPasswordInput" placeholder="Password" class="w-full rounded-md text-lg outline-none border-2 p-1">
     <input name="confirmPassword" type="password" placeholder="Confirmer le mot de passe" class="w-full rounded-md text-lg outline-none border-2 p-1">
+    <p class="mr-auto" id="passwordStrengthText">Complexitée du mot de passe :</p>
+    <div class="w-full h-3 rounded-md border-2 flex">
+        <div class="tick border-r-2 w-1/4 h-full rounded-l-md"></div>
+        <div class="tick border-r-2 w-1/4 h-full"></div>
+        <div class="tick border-r-2 w-1/4 h-full"></div>
+        <div class="tick border-r-2 w-1/4 h-full rounded-r-md"></div>
+    </div>
     <button class="border-none bg-transparent mr-auto"><a href="login.php">Vous avez déjà un compte ?</a></button>
     <button class="bg-[#b2a5ff] p-2 w-full rounded-md">Inscription</button>
     <div class="h-px w-full bg-[#e8eaed]"></div>
@@ -51,5 +71,6 @@ if(isset($_POST["email"]) && isset($_POST["username"]) && isset($_POST["password
         <button><img src="/assets/icons/linkedin.png" alt="linkedin icons" class="w-8 h-8 rounded-full"></button>
     </div>
 </form>
+<script src="../../scripts/admin/register.js"></script>
 </body>
 </html>
