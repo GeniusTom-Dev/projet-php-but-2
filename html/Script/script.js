368a8cd2369ea04327286129107677fc43818642
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-11-03 14:59:31 +0100

    Modification de la bio et de la PP

--- html/Script/script.js
+++ html/Script/script.js
@@ -1,28 +1,21 @@
-let growArrow = document.querySelector("#growArrow");
-let article = document.querySelector('.article');
-
-let heartRegular = document.querySelector("#heartRegular");
-let bookmarkRegular = document.querySelector("#bookmarkRegular");
 
 // Variable pour suivre l'état actuel de l'agrandissement
-let isEnlarged = false;
-
-let commentButton = document.querySelector("#comment-button");
-let commentInput = document.querySelector("#comment-input");
-let commentsContainer = document.querySelector("#comment-section");
-
 let plusButton = document.querySelector("#plusButton");
 let fileInput = document.querySelector("#fileInput");
+const profilePicture = document.getElementById("profilePicture");
+const profileImage = document.getElementById("profileImage");
+let fileInputPP = document.querySelector("#fileInputPP");
 
 let suscribeButton = document.querySelector("#suscribe-button");
 
-// Variable pour suivre l'état actuel de l'abonnement
-let isSubscribed = false;
-
+// Variable bouton supprimer, et biographie
 let trashCan = document.querySelector("#trashCan");
-let deleteConfirmation = document.querySelector("#deleteConfirmation");
-let confirmDeleteButton = document.querySelector("#confirmDeleteButton");
-let cancelDeleteButton = document.querySelector("#cancelDeleteButton");
+const bioContainer = document.getElementById("bioContainer");
+    const bioForm = document.getElementById("bioForm");
+    const userBio = document.getElementById("userBio");
+    const editButton = document.getElementById("editButton");
+    const editForm = document.getElementById("editForm");
+    const bioTextArea = document.getElementById("bioTextArea");
 
 
 plusButton.addEventListener("click", () => {
@@ -40,11 +33,11 @@ fileInput.addEventListener("change", (event) => {
         // Créez un élément img pour afficher l'image
         const imgElement = document.createElement("img");
         imgElement.src = URL.createObjectURL(selectedFile);
-        imgElement.className = "w-32 h-auto"; // Ajustez la taille de l'image selon vos besoins
+        imgElement.className = "w-64 h-auto p-2 "; // Ajustez la taille de l'image selon vos besoins
 
         // Créez un bouton de suppression pour l'image
         const deleteButton = document.createElement("button");
-        deleteButton.style.background = "url('/Projet/projet-php-but-2/html/images/trash-can-solid.svg')"; /* Remplacez 'votre-image.png' par le chemin de votre image */
+        deleteButton.style.background = "url('/html/images/trash-can-solid.svg')"; /* Remplacez 'votre-image.png' par le chemin de votre image */
         //deleteButton.style.backgroundSize = "cover"; /* Ajustez la taille de l'image selon vos besoins */
         deleteButton.style.padding = "10px 9px";
         deleteButton.addEventListener("click", () => {
@@ -62,3 +55,59 @@ fileInput.addEventListener("change", (event) => {
     }
 });
 
+     // Fonction pour gérer l'état de modification
+     function toggleEditMode() {
+        const isEditing = bioForm.style.display === "block";
+        bioForm.style.display = isEditing ? "none" : "block";
+        bioContainer.style.display = isEditing ? "block" : "none";
+        editButton.style.display = isEditing ? "block" : "none";
+      }
+  
+ 
+      // Afficher le formulaire de modification de la bio
+      editButton.addEventListener("click", () => {
+        toggleEditMode();
+      });
+      bioTextArea.addEventListener('input', () => {
+        const maxLength = bioTextArea.maxLength;
+        const currentLength = bioTextArea.value.length;
+        const remainingCharacters = maxLength - currentLength;
+        
+        charCount.textContent = `Caractères restants : ${remainingCharacters}`;
+    });
+  
+      // Enregistrer la bio modifiée
+      editForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const newBio = bioTextArea.value;
+        userBio.textContent = newBio;
+        const maxLength = bioTextArea.maxLength;
+        const currentLength = bioTextArea.value.length;
+        
+        if (currentLength > maxLength) {
+            alert('La biographie dépasse la limite de caractères autorisée.');
+            e.preventDefault(); // Empêcher la soumission du formulaire
+        }
+        toggleEditMode();
+      });
+    
+       // Gérer le clic sur la photo de profil pour ouvrir le sélecteur de fichier
+    profilePicture.addEventListener("click", () => {
+        fileInputPP.click();
+      });
+  
+      // Gérer le changement de la photo de profil lors de la sélection d'un fichier
+      fileInputPP.addEventListener("change", (event) => {
+        const selectedFile = event.target.files[0];
+  
+        if (selectedFile) {
+          const profileImage = document.getElementById("profileImage");
+  
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            profileImage.src = e.target.result;
+          };
+          reader.readAsDataURL(selectedFile);
+          
+        }
+      });
--- pageProfil.php
+++ pageProfil.php
@@ -5,7 +5,6 @@ function start_page($title): void
 <html lang="fr">
     <head>
         <title><?php echo $title; ?></title>
-        <!--<link rel="stylesheet" href="/Projet/projet-php-but-2/navbar.css"/>-->
         <script src="https://cdn.tailwindcss.com"></script>
         
         
@@ -22,27 +21,54 @@ function start_page($title): void
         function end_page($title): void
         {
         ?>
-        <body>
             <hr><br><strong><?php echo $title; ?></strong><br><hr>
         <?php
         }
-        ?>
+        
+        $name = "Zoro_13";
+        /*Follow représente le nombre de profil qui suivent l'utilisateur(lien avec la BD) */
+        $Follow = 320;
+        $abonnement = 800;
+        $dernierConnenxion = "02/11/2023";
+
+?>
 <div class="flex">
     <div class="min-h-screen flex-1 flex items-center justify-center bg-gray-200">
         <!-- Conteneur principal de la page -->
         <div class="article w-2/4 h-100 bg-gray-100 rounded-xl p-6 shadow-xl">
             <!-- En-tête de l'article (profil utilisateur) -->
             <header class="article-header relative mb-8">
-                <!-- Image de profil utilisateur -->
-                <img src="/zoro.png" alt="PP" class="article-image absolute top-0 left-8 w-40 h-40 rounded-full transition-transform hover:scale-125">
+               
+            <!-- Image de profil utilisateur -->
+            <div class="profile-picture" id="profilePicture">    
+            <img src="/html/images/profile-removebg-preview.png" alt="Photo de profil" id="profileImage"  class="article-image absolute top-0 left-8 w-40 h-40 rounded-full transition-transform hover:scale-125">
+            </div>  
+            <!-- Formulaire pour sélectionner un nouveau fichier de photo de profil -->
+                <input type="file" id="fileInputPP" style="display: none" accept="image/*">
                 <!-- Informations de profil -->
                 <div class="pl-64">
-                    <p class="text-xl font-bold"><?php echo "Zoro_13"; ?></p>
-                    <p class="mb-2"><?php echo "Follow | 320 followers"; ?></p>
-                    <p class="mb-2"><?php echo "Abonnements : 800"; ?></p>
-                    <p class="mb-2"><?php echo "Dernière connexion : ..."; ?></p>
-                    <p class="mb-2"><?php echo "Bio : La bio de mon profil"; ?></p>
-                </div>
+                    <p class="text-xl font-bold"><?php echo $name; ?></p>
+                    <p class="mb-2"><?php echo "Follow ". $Follow ;?></p>
+                    <p class="mb-2"><?php echo "Abonnement ".$abonnement; ?></p>
+                    <p class="mb-2"><?php echo "Dernière connexion : ". $dernierConnenxion; ?></p>
+                    <!-- Section de la biographie et du formulaire de modification -->
+<div class="bio" id="bioContainer">
+    <h1>Ma Biographie :</h1>
+    <p id="userBio">Biographie actuelle de l'utilisateur</p>
+</div>
+
+<div class="bio-form" id="bioForm" style="display: none;">
+    <form id="editForm">
+        <textarea id="bioTextArea" rows="3" cols="20" placeholder="Saisissez votre biographie ici" maxlength="200"></textarea>
+        <p id="charCount">Caractères restants : 200</p>
+        <br>
+        <input type="submit" value="Enregistrer">
+    </form>
+</div>
+
+<!-- Bouton pour modifier la biographie -->
+<button id="editButton">Modifier Ma Biographie</button>
+
             </header>
             <!-- Section principale de l'article -->
             <main class="border-2 border-[#b2a5ff]-500 p-0.5 px-14 flex items-center justify-between flex-col">
@@ -51,21 +77,17 @@ function start_page($title): void
                 <div class="flex items-center space-x-4">
                     <!-- Conteneur d'images -->
                     <div id="imageContainer" class="mt-4">
-                        <!-- Bouton pour ajouter une image -->
-                        <button id="plusButton" class="w-4 h-auto transform transition-transform duration-300 hover:scale-125">
-                            <!-- Image de bouton "plus" -->
-                            <img src="/html/images/plus-solid.svg" alt="plus">
-                        </button>
                         <!-- Input de type "file" caché pour télécharger des images -->
                         <input type="file" id="fileInput" accept="image/*" style="display: none;" class ="p-6">
                         <!-- Conteneur de galerie pour afficher les images ajoutées -->
                         <div id="galleryContainer" class="mt-4"></div>
                     </div>
                 </div>
-                <!-- Section vide pour afficher les images sélectionnées -->
-                <div class="py-8">
-                    <div>
-                    </div>
+                 <!-- Bouton pour ajouter une image -->
+                    <button id="plusButton" class="w-4 h-auto transform transition-transform duration-300 hover:scale-125">
+                        <!-- Image de bouton "plus" -->
+                        <img src="/html/images/plus-solid.svg" alt="plus">
+                    </button>
                 </div>
             </main>
         </div>
