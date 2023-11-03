d7a15e8f5a1372741a75faff187cef2ac3f5954b
maxime <maxime.jauras@etu.univ-amu.fr>
2023-11-03 15:38:24 +0000

    Update design user search bar

--- GFramework/searchBar/searchBar.php
+++ GFramework/searchBar/searchBar.php
@@ -23,7 +23,7 @@ if (empty($_GET["selectDb"]) === false) {
 <div class="max-w-3xl mx-auto p-4">
     <form method="GET" id="searchForm" class="space-y-4">
         <div class="flex flex-lign items-center mb-2">
-            <select id="selectDb" name="selectDb" class="w-1/4 bg-gray-100 p-2 rounded-md mr-4">
+            <select id="selectDb" name="selectDb" class="w-1/4 h-8 bg-gray-100 p-2 border border-[#b2a5ff] rounded-md mr-4 pt-1 pb-1">
                 <option value="Topics" <?php if ($_GET['selectDb'] == 'Topics') echo 'selected'; ?>>Categories</option>
                 <option value="Users" <?php if ($_GET['selectDb'] == 'Users') echo 'selected'; ?>>Utilisateurs</option>
                 <option value="Posts" <?php if ($_GET['selectDb'] == 'Posts') echo 'selected'; ?>>Posts</option>
@@ -31,9 +31,9 @@ if (empty($_GET["selectDb"]) === false) {
             </select>
             <input type="text" id="searchText" name="searchText" placeholder="Rechercher..."
                 <?php if (empty($_GET['searchText']) === false) echo 'value="' . $_GET["searchText"] . '"'; ?>
-                   class="w-3/4 bg-gray-100 p-2 rounded-md mr-2"/>
+                   class="w-2/3 h-8 bg-gray-100 p-2 border border-[#b2a5ff] rounded-md mr-2"/>
             <input type="submit" value="Rechercher" id="search"
-                   class="bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700"/>
+                   class="w-32 h-8 pb-1 pt-1 bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700"/>
             <div id="searchFilters">
                 <?php if (!isset($_GET['selectDb']) || $_GET['selectDb'] == "Topics") getTopicsFilters(false);
                 else if ($_GET['selectDb'] == "Users") getUsersFilters(false);
