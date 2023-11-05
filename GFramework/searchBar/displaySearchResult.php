bb7c5c9d56701084a428f911a5f34339d59093de
Badlix <mathieu.leroux173@gmail.com>
2023-11-05 03:02:39 +0100

    add User search result display

--- GFramework/searchBar/displaySearchResult.php
+++ GFramework/searchBar/displaySearchResult.php
@@ -3,22 +3,10 @@ require_once __DIR__ .'/../autoloader.php';
 
 
 function whatToDisplay($dbComments, $dbFavorites, $dbFollows, $dbLikes, $dbPosts,$dbTopics, $dbUsers) {
-
-    /*$db = new Database('mysql-echo.alwaysdata.net', 'echo_mathieu', '130304leroux', 'echo_bd');
-    $dbConn = $db->getConnection()->getContent();
-
-    $dbComments = new DbComments($dbConn);
-    $dbFavorites = new DbFavorites($dbConn);
-    $dbFollows = new DbFollows($dbConn);
-    $dbLikes = new DbLikes($dbConn);
-    $dbPosts = new DbPosts($dbConn);
-    $dbTopics = new DbTopics($dbConn);
-    $dbUsers = new DbUsers($dbConn);*/
-
     $results = [];
     if (empty($_GET["selectDb"]) === false) {
         if ($_GET["selectDb"] == "Topics") $results = getTopicsResults($dbTopics);
-        else if ($_GET["selectDb"] == "Users") $results = getUsersResults($dbUsers);
+        else if ($_GET["selectDb"] == "Users") displayUsers(getUsersResults($dbUsers), $dbFollows, $dbUsers);
         else if ($_GET["selectDb"] == "Posts") displayPosts(getPostsResults($dbPosts, $dbTopics), $dbComments, $dbFavorites, $dbFollows, $dbLikes, $dbPosts,$dbTopics, $dbUsers);
         else if ($_GET["selectDb"] == "Comments") $results = getCommentsResults($dbComments);
     } else {
@@ -36,6 +24,16 @@ function displayPosts($searchResult, $dbComments, $dbFavorites, $dbFollows, $dbL
     $htmlCode = "";
     foreach ($searchResult as $post) {
         $htmlCode .= '<tr>' . $controller->getPostHTML($post["POST_ID"]) . '</tr>';
+    }
+    echo $htmlCode;
+}
+
+function displayUsers($searchResult, $dbFollows, $dbUsers) : void {
+    $controller = new \controllers\controlUser($dbFollows, $dbUsers);
+    $controller->checkSubscribe();
+    $htmlCode = "";
+    foreach ($searchResult as $user) {
+        $htmlCode .= '<tr>' . $controller->getUserProfileSimple($user["USER_ID"]) . '</tr>';
     }
     echo $htmlCode;
 }
