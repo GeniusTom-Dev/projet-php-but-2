69af5ffdee6ded745aa3edf546acfbf60d080c90
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-11-05 14:25:32 +0100

    Alignes en colone le topic

--- controllers/controlTopic.php
+++ controllers/controlTopic.php
@@ -21,7 +21,7 @@ class controlTopic
         $topicData = $this->dbTopics->selectById($topicId)->getContent();
         ob_start(); ?>
         <section
-                class="topiceSimple flex flex-lign items-center w-full md:w-1/2 lg:w-1/3 xl:w-1/2 h-auto md:h-1/3 lg:h-auto xl:h-auto bg-gray-100 rounded-lg shadow-md p-6 mb-4">
+                class="topiceSimple flex flex-col items-center w-full md:w-1/2 lg:w-1/3 xl:w-1/2 h-auto md:h-1/3 lg:h-auto xl:h-auto bg-gray-100 rounded-lg shadow-md p-6 mb-4">
             <form action="" method="get">
                 <input type="hidden" name="topicData" value="<?= $topicData['TOPIC_ID'] ?>">
                 <p><?php echo $topicData['NAME'] ?></p>
