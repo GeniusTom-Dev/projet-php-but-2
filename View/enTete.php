48015fae9181cb1d6d1c6a3131a2ff3bb92cc41e
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-11-05 18:28:38 +0100

    Centrer barre de recherche

--- View/enTete.php
+++ View/enTete.php
@@ -1,8 +1,8 @@
-<header class="flex flex-lign items-center mb-2 bg-purple-800 p-2">
-    <div class="w-100 h-100">
-        <img src="/projet-php-but-2/html/images/profile-removebg-preview.png" alt="PP" class="w-20 h-auto transition-transform duration-300 hover:scale-125 mr-1">
-    </div>
-    <div class="flex justify-center items-center ml-16">
+<header class="flex justify-between items-center mb-2 bg-purple-800 p-2">
+    <div class="w-20 h-auto transition-transform duration-300 hover:scale-125 ml-1">
+        <img src="/projet-php-but-2/html/images/profile-removebg-preview.png" alt="PP">
+    </div>    
+    <div class="mx-auto flex items-center ">
         <?php include '../GFramework/searchBar/searchBar.php' ?>
     </div>
 </header>
