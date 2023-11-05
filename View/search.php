11978536a1f3b2692952634bd5109a6e50dcbada
maxime <maxime.jauras@etu.univ-amu.fr>
2023-11-05 11:52:01 +0000

    Update display button previous page and next page

--- View/search.php
+++ View/search.php
@@ -17,39 +17,57 @@ require_once "enTete.php";
 require_once "navbarTailswind.php";
 
 require_once '../GFramework/searchBar/displaySearchResult.php';
-
-// Bouton page précédente
-if ($_GET['page'] > 1) { ?>
-    <div>
-        <form method="get">
-            <button name="page" value="<?= $_GET['page'] - 1 ?>">Page précédente</button>
-        </form>
-    </div>
-<?php } ?>
-
+?>
 
 <div class="flex flex-col items-center mb-8">
+    <div class="flex flex-lign items-center mb-4">
+        <?php if ($_GET['page'] > 1) { ?>
+            <div>
+                <form method="get">
+                    <button name="page" value="<?= $_GET['page'] - 1 ?>" class="bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700 mr-2">Page précédente</button>
+                </form>
+            </div>
+        <?php } ?>
+
+        <?php
+        $max = getTotal($dbComments, $dbPosts, $dbTopics, $dbUsers);
+        if ($max % $limitRows != 0) {
+            $max = (int)($max / $limitRows) + 1;
+        } else {
+            $max = (int)($max / $limitRows);
+        }
+        
+        if ($_GET['page'] < $max) { ?>
+        <div>
+            <form method="get">
+                <button name="page" value="<?= $_GET['page'] + 1 ?>" class="bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700">Page suivante</button>
+            </form>
+        </div>
+        <?php } ?>
+    </div>
+
     <h2>Resultats de la recherche :</h2>
     <table id="table">
         <?php whatToDisplay($dbComments, $dbFavorites, $dbFollows, $dbLikes, $dbPosts, $dbTopics, $dbUsers, $limitRows, $_GET['page'], 'recent'); ?>
     </table>
-</div>
 
-<?php
-$max = getTotal($dbComments, $dbPosts, $dbTopics, $dbUsers);
-if ($max % $limitRows != 0) {
-    $max = (int)($max / $limitRows) + 1;
-} else {
-    $max = (int)($max / $limitRows);
-}
-
-if ($_GET['page'] < $max) { ?>
-    <div>
-        <form method="get">
-            <button name="page" value="<?= $_GET['page'] + 1 ?>">Page suivante</button>
-        </form>
+    <div class="flex flex-lign items-center mb-4">
+        <?php if ($_GET['page'] > 1) { ?>
+            <div>
+                <form method="get">
+                    <button name="page" value="<?= $_GET['page'] - 1 ?>" class="bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700 mr-2">Page précédente</button>
+                </form>
+            </div>
+        <?php }
+         if ($_GET['page'] < $max) { ?>
+            <div>
+                <form method="get">
+                    <button name="page" value="<?= $_GET['page'] + 1 ?>" class="bg-purple-500 text-white p-2 rounded-md hover:bg-purple-700">Page suivante</button>
+                </form>
+            </div>
+        <?php } ?>
     </div>
-<?php } ?>
+</div>
 <script src="../html/script/scriptShowPost.js"></script>
 
 
